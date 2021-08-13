4f592885f69d0d93ebd9f8538b5178df404bf279
myase <myase@DESKTOP-7U6D02F>
2021-08-12 21:24:50 -0400

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthOfSecondElapsed;
+        int matchesFound;
+            public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += Timer_Tick;
+            SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐵", "🐵",
+                "🐶", "🐶",
+                "🐺", "🐺",
+                "🐎", "🐎",
+                "🦓", "🦓",
+                "🐷", "🐷",
+                "🐪", "🐪",
+                "🐇", "🐇"
+
+
+            };
+            Random random = new Random();
+
+            foreach (TextBlock textblock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textblock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+
+            }
+            Console.ReadLine();
+        }
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if(findingMatch== false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+
+            }
+            else if(textBlock.Text== lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+    }
+}
